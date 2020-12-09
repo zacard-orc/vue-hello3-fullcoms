c636b2d42b933424d36d6553197cc9d7b717e10f
13816025562@139.com <13816025562@139.com>
2020-12-09 15:19:39 +0800

    ft: slim-3

--- examples/plugins/xtable/index.js
+++ examples/plugins/xtable/index.js
@@ -3,32 +3,8 @@ import i18n from '@/i18n'
 
 import {
   VXETable,
-  Table,
-  Column,
-  Header,
-  Footer,
-  Filter,
-  Grid,
-  Menu,
-  Toolbar,
-  Pager,
-  Checkbox,
   Radio,
-  Input,
-  Textarea,
-  Button,
-  Modal,
-  Tooltip,
-  Form,
-  Select,
-  Switch,
-  Edit,
-  List,
-
-  Export,
-  Keyboard,
-  Validator,
-  Resize
+  Button
 } from '../../../packages/vxe-table'
 
 // import VXETablePluginElement from '../../../../vxe-table-plugin-element/test.js'
@@ -39,24 +15,24 @@ import {
 // import VXETablePluginMenus from '../../../../vxe-table-plugin-menus/test.js'
 // import VXETablePluginCharts from '../../../../vxe-table-plugin-charts/test.js'
 // import VXETablePluginShortcutKey from '../../../../vxe-table-plugin-shortcut-key/test.js'
-import VXETablePluginElement from 'vxe-table-plugin-element'
-import VXETablePluginIView from 'vxe-table-plugin-iview'
-import VXETablePluginAntd from 'vxe-table-plugin-antd'
-import VXETablePluginExcel from 'vxe-table-plugin-excel'
-import VXETablePluginRenderer from 'vxe-table-plugin-renderer'
-import VXETablePluginVirtualTree from 'vxe-table-plugin-virtual-tree'
-import VXETablePluginMenus from 'vxe-table-plugin-menus'
-import VXETablePluginCharts from 'vxe-table-plugin-charts'
-import VXETablePluginExportXLSX from 'vxe-table-plugin-export-xlsx'
-import VXETablePluginExportPDF from 'vxe-table-plugin-export-pdf'
+// import VXETablePluginElement from 'vxe-table-plugin-element'
+// import VXETablePluginIView from 'vxe-table-plugin-iview'
+// import VXETablePluginAntd from 'vxe-table-plugin-antd'
+// import VXETablePluginExcel from 'vxe-table-plugin-excel'
+// import VXETablePluginRenderer from 'vxe-table-plugin-renderer'
+// import VXETablePluginVirtualTree from 'vxe-table-plugin-virtual-tree'
+// import VXETablePluginMenus from 'vxe-table-plugin-menus'
+// import VXETablePluginCharts from 'vxe-table-plugin-charts'
+// import VXETablePluginExportXLSX from 'vxe-table-plugin-export-xlsx'
+// import VXETablePluginExportPDF from 'vxe-table-plugin-export-pdf'
 // import VXETablePluginShortcutKey from 'vxe-table-plugin-shortcut-key'
-import 'vxe-table-plugin-element/dist/style.css'
-import 'vxe-table-plugin-iview/dist/style.css'
-import 'vxe-table-plugin-antd/dist/style.css'
-import 'vxe-table-plugin-excel/dist/style.css'
-import 'vxe-table-plugin-virtual-tree/dist/style.css'
-import 'vxe-table-plugin-renderer/dist/style.css'
-import 'vxe-table-plugin-charts/dist/style.css'
+// import 'vxe-table-plugin-element/dist/style.css'
+// import 'vxe-table-plugin-iview/dist/style.css'
+// import 'vxe-table-plugin-antd/dist/style.css'
+// import 'vxe-table-plugin-excel/dist/style.css'
+// import 'vxe-table-plugin-virtual-tree/dist/style.css'
+// import 'vxe-table-plugin-renderer/dist/style.css'
+// import 'vxe-table-plugin-charts/dist/style.css'
 
 // 设置默认参数
 VXETable.setup({
@@ -70,47 +46,14 @@ VXETable.setup({
 })
 
 // 先安装依赖模块
-Vue.use(Column)
-Vue.use(Header)
-Vue.use(Footer)
-Vue.use(Filter)
-Vue.use(Grid)
-Vue.use(Menu)
-Vue.use(Toolbar)
-Vue.use(Pager)
-Vue.use(Checkbox)
 Vue.use(Radio)
-Vue.use(Input)
-Vue.use(Textarea)
 Vue.use(Button)
-Vue.use(Modal)
-Vue.use(Tooltip)
-Vue.use(Form)
-Vue.use(Select)
-Vue.use(Switch)
-Vue.use(List)
 
-Vue.use(Edit)
-Vue.use(Export)
-Vue.use(Keyboard)
-Vue.use(Validator)
-Vue.use(Resize)
 // 再安装核心
-Vue.use(Table)
 
 // 给 vue 实例挂载全局窗口对象
 Vue.prototype.$XModal = VXETable.modal
 
-VXETable.use(VXETablePluginElement)
-VXETable.use(VXETablePluginIView)
-VXETable.use(VXETablePluginAntd)
-VXETable.use(VXETablePluginExcel)
-VXETable.use(VXETablePluginRenderer)
-VXETable.use(VXETablePluginVirtualTree)
-VXETable.use(VXETablePluginMenus)
-VXETable.use(VXETablePluginCharts)
-VXETable.use(VXETablePluginExportXLSX)
-VXETable.use(VXETablePluginExportPDF)
 // VXETable.use(VXETablePluginShortcutKey, {
 //   disabled: ['ArrowLeft'],
 //   listener: {
